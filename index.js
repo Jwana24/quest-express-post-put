5964fce4d93a2dbc152b9c49fbde5f6577e5f3ee
Jwana24 <detrieux.btscom13@gmail.com>
2020-05-08 22:52:22 +0200

    add input validations with express-validator module

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const connection = require('./db');
 const app = express();
 
+app.use(express.json());
+
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -25,31 +27,64 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-app.post('/api/users', (req, res) => {
+// ajout d'un user avec vérifications manuelles des champs
+// app.post('/api/users', (req, res) => {
+//   const dataUser = req.body;
+
+//   const { email, password, name } = req.body;
+//   if(!email || !password || !name){
+//     return res.status(422).json({
+//       error: 'Missing at least one of the required fields is missing'
+//     });
+//   }
+
+//   const emailRegex = /[a-z0-9._-]+@[a-z0-9-]+\.[a-z]{2,6}/;
+//   // si l'email ne passe pas le test de la regex, l'erreur est affichée
+//   if(!emailRegex.test(email)){
+//     return  res.status(422).json({
+//       error: 'Invalid email',
+//     })
+//   }
+
+//   const passwordRegex = /^((?=.*[@#!$&+%/*])(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,})$/;
+//   if(!passwordRegex.test(password)){
+//     return res.status(422).json({
+//       error: 'Password too short (8 characters min.) !',
+//     })
+//   }
+
+//   connection.query('INSERT INTO user SET ?', dataUser, (err, results) => {
+//     if(err){
+//       console.log(err)
+//       res.status(500).json({
+//         error: err.message,
+//         sql: err.sql,
+//       });
+//     }
+//     else{
+//       res.json(results);
+//     }
+//   })
+// });
+
+// make validations on the inputs
+const { check, validationResult } = require('express-validator');
+
+// ajout d'un user avec vérifications des champs par le module "express-validator"
+// ce module utilise le système de "middlewares" en rajoutant des propriétés à "req"
+app.post('/api/users', [
+  check('email').isEmail(),
+  check('password').isLength({ min: 8 }),
+  check('name').isLength({ min: 4 })
+], (req, res) => {
+
+  // Finds the validation errors in this request and wraps them in an object with handy functions
+  const errors = validationResult(req);
+  if(!errors.isEmpty()){
+    return res.status(422).json({ errors: errors.array() });
+  }
+  
   const dataUser = req.body;
-
-  const { email, password, name } = req.body;
-  if(!email || !password || !name){
-    return res.status(422).json({
-      error: 'Missing at least one of the required fields is missing'
-    });
-  }
-
-  const emailRegex = /[a-z0-9._-]+@[a-z0-9-]+\.[a-z]{2,6}/;
-  // si l'email ne passe pas le test de la regex, l'erreur est affichée
-  if(!emailRegex.test(email)){
-    return  res.status(422).json({
-      error: 'Invalid email',
-    })
-  }
-
-  const passwordRegex = /^((?=.*[@#!$&+%/*])(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,})$/;
-  if(!passwordRegex.test(password)){
-    return res.status(422).json({
-      error: 'Password too short (8 characters min.) !',
-    })
-  }
-
   connection.query('INSERT INTO user SET ?', dataUser, (err, results) => {
     if(err){
       console.log(err)
@@ -62,7 +97,7 @@ app.post('/api/users', (req, res) => {
       res.json(results);
     }
   })
-});
+})
 
 app.listen(process.env.PORT, (err) => {
   if (err) {
